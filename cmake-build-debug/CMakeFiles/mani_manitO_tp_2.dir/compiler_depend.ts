d6ac3fa7826dc09fa8e2279a45a6ebba7c0d8ef2
agustín colman <agustincolman0310@gmail.com>
2022-06-01 16:34:41 -0300

    vaciar_lista() en lecturas

---
+++ cmake-build-debug/CMakeFiles/mani_manitO_tp_2.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for mani_manitO_tp_2.
---
+++ cmake-build-debug/CMakeFiles/mani_manitO_tp_2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mani_manitO_tp_2.
+# This may be replaced when dependencies are built.
